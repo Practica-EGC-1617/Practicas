d656d77c916ac690cf43ae6a280f0b18402e6b14
Pablo <pabloyandrak@gmail.com>
2016-10-26 17:00:23 +0200

    Commit uno

---
+++ src/src/Suma.java
@@ -0,0 +1,19 @@
+package src;
+
+import org.junit.Test;
+
+public class Suma {
+
+	public int SumaContador(){
+		int i = 2;
+		i=i+4;
+		return i;
+	}
+	
+	@Test
+	public int sum(int i, int j){
+		return i+j;
+	}
+	
+	
+}
